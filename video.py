873a968460bc3950f6f69dbeedf700fbf72aca76
Tyler Estes <estysdesu@gmail.com>
2019-05-15 20:22:28 +0900

    init commit

---
+++ video.py
@@ -0,0 +1,12 @@
+import moviepy.editor as mp
+
+filePath = r"courtchanges20183.mp4"
+videoOrig = mp.VideoFileClip(filePath)
+
+videoText = (mp.TextClip("5432", fontsize=150, color="white", stroke_width=5)
+        .set_position(("right", "top"))
+        .margin(right=50, top=50, opacity=0) # (optional) logo-border padding
+        .set_duration(videoOrig.duration)
+        )
+videoOverlay = mp.CompositeVideoClip([videoOrig, videoText])
+videoOverlay.subclip(300, 350).write_videofile("videoOverlay.mp4")
