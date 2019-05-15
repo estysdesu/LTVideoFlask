dbdfab1d6924f5b7148d2c0b7cfc9641f2488410
Tyler Estes <estysdesu@gmail.com>
2019-05-15 20:46:35 +0900

    fixed requirements.txt doubles

--- video.py
+++ video.py
@@ -3,7 +3,7 @@ import moviepy.editor as mp
 filePath = r"courtchanges20183.mp4"
 videoOrig = mp.VideoFileClip(filePath)
 
-videoText = (mp.TextClip("5432", fontsize=150, color="white", stroke_width=50)
+videoText = (mp.TextClip("5432", font="arial", fontsize=150, color="white", stroke_width=25)
         .set_position(("right", "top"))
         .margin(right=50, top=50, opacity=0) # (optional) logo-border padding
         .set_duration(videoOrig.duration)
