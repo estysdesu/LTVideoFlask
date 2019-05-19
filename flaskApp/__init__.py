069c77e0bbc719d0fcf64d0a74552a0ff1f7e9c9
Tyler Estes <estysdesu@gmail.com>
2019-05-19 14:51:23 +0900

    add file types to .gitignore

--- flaskApp/__init__.py
+++ flaskApp/__init__.py
@@ -5,9 +5,7 @@ import os
 FlaskApp = Flask(__name__, instance_relative_config=True)
 
 # config
-FlaskApp.config.from_object("config")
-if os.path.exists("instance"):
-    FlaskApp.config.from_pyfile("config.py")
+# FlaskApp.config.from_object("config")
+# if os.path.exists("instance"):
+#     FlaskApp.config.from_pyfile("config.py")
 
-# import modules
-from .. import videoProcessing
